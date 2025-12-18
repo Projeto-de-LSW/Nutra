8a9efe240bf692c921a11354cd5c72c05b83ad5b
Brunacl <brunalacerdacl@gmail.com>
2025-12-18 13:07:32 -0300

    Update loginCadastro display and text for logged in user

--- Nutra-main/JS/usuario_logado.js
+++ Nutra-main/JS/usuario_logado.js
@@ -2,5 +2,5 @@ const loginCadastro = document.querySelector(".menu #logincadastro")
 const usuario = localStorage.getItem('usuarioLogado')
 
 if(usuario != null) {
-    loginCadastro.style.display = 'none'
+    loginCadastro.innerHTML = 'Cadastrado'
 }
